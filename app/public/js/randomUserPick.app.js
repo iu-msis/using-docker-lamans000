6462de277617c5cdf7d8626e720c13c46945bdda
lamans000 <ansonlam640@gmail.com>
2019-09-15 21:31:10 -0400

    Got rid of the errors

--- app/public/js/randomUserPick.app.js
+++ app/public/js/randomUserPick.app.js
@@ -1,13 +1,62 @@
 var waitingApp = new Vue({
   el: '#randomBioSelection',
   data: {
-    person:[{
+    person:{
+          "gender": "",
+          "name": {
+            "title": "",
+            "first": "",
+            "last": ""
+          },
+          "location": {
+            "street": "",
+            "city": "",
+            "state": "",
+            "postcode": "",
+            "coordinates": {
+              "latitude": "",
+              "longitude": ""
+            },
+            "timezone": {
+              "offset": "",
+              "description": ""
+            }
+          },
+          "email": "",
+          "login": {
+            "uuid": "",
+            "username": "",
+            "password": "",
+            "salt": "",
+            "md5": "",
+            "sha1": "",
+            "sha256": ""
+          },
+          "dob": {
+            "date": "",
+            "age": null
+          },
+          "registered": {
+            "date": "",
+            "age": null
+          },
+          "phone": "",
+          "cell": "",
+          "id": {
+            "name": "",
+            "value": ""
+          },
+          "picture": {
+            "large": "",
+            "medium": "",
+            "thumbnail": ""
+          },
+          "nat": ""
     }
-  ]
   },
 
   methods: {
-    fetchPatients() {
+    fetchPersons() {
       fetch('https://randomuser.me/api')
       .then(response => response.json())
       .then(json => waitingApp.person = json.results[0]);
@@ -20,6 +69,6 @@ var waitingApp = new Vue({
     }
   },
   created() {
-    this.fetchPatients()
+    this.fetchPersons();
   }
-});
+})
